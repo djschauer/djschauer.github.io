672e03cacaf082b240e8eb7d1f5f31bb4becc1ab
Merlin <daniel.schauer93@gmail.com>
2018-07-31 23:25:44 -0500

    spamazon

--- Spamazon/spamazon.js
+++ Spamazon/spamazon.js
@@ -47,16 +47,14 @@ var connection = mysql.createConnection({
 
   function buyMode() {
         console.log("These are the items currently available");
-        connection.query("SELECT itemName, department, price, stock FROM inventoryCatalog", (err, res)=>{
-            table.draw(res);
-        });
+        displayInventory();
         inquirer.prompt(
             [
                 {
                     name: "item",
                     type: "input",
                     message: "What item would you like to buy?"
-                }
+                },
                 {
                     name: "quantity",
                     type: "input",
@@ -66,10 +64,60 @@ var connection = mysql.createConnection({
         ).then(function(answers) {
             var itemID = answers.item;
             var amount = answers.quantity;
+            connection.query("SELECT * FROM inventoryCatalog WHERE ?", 
+            {
+                id: itemID
+            }, function(err, res) {
+                if (err) throw err;
+                console.log("Processing Order");
+                var item = res[0];
+                var itemStock = item.stock;
+                var itemPrice = item.price;
+                var productSold = item.productSold;
 
-            connection.query("")            
-            console.log("You have bought: " + answers.quantity + " " + answers.item);
+                console.log(itemStock);
+                
+                if (res.length === 0) {
+                    console.log("There are no products with that ID. Please select a valid product ID");
+                    buyMode();
+                } else if (itemStock < amount) {
+                    console.log("There are not enough items in stock for you to purchase that. Please either select a new item, or buy less.");
+                    } else {
+                        console.log("Placing Order");
+                        connection.query("UPDATE inventoryCatalog SET ? WHERE ?",
+                    [ 
+                        {
+                            stock: itemStock - amount,
+                            productSold: productSold + amount                        
+                        },
+                        {
+                            id: itemID
+                        }
+                    ], function (err, res) {
+                        if (err) throw err;
 
+                        console.log("Your order has been placed.")
+                        console.log("You have been charged: " + (amount * itemPrice));
+                        displayInventory();
+                        inquirer.prompt(
+                            [
+                                {
+                                    name: "buyagain",
+                                    type: "confirm",
+                                    message: "Would you like to continue shopping?",
+                                    default: true
+                                }
+                            ]
+                        ).then(function(answers){
+                            if (answers) {
+                                buyMode();
+                            } else {
+                                process.exit();
+                            }
+                        });
+                    });
+                }
+            });
         });
   };
 
@@ -79,4 +127,10 @@ var connection = mysql.createConnection({
 
   function salesReports() {
 
+  };
+
+  function displayInventory () {
+    connection.query("SELECT id, itemName, department, price, stock FROM inventoryCatalog", (err, res)=>{
+        table.draw(res);
+    });
   };
