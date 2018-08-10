5bea01409a96c75342fd9530c2ad1f5f7b7e713a
Merlin <daniel.schauer93@gmail.com>
2018-08-09 20:50:00 -0500

    herou and activities update

--- friendFinder/server.js
+++ friendFinder/server.js
@@ -5,7 +5,7 @@ var html = require("./htmlRoutes");
 
 var app = express();
 
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
